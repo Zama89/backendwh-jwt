84566809f2bda7b78c1a20be6fbe08f3b2643c08
Jon Moliner <jonmolinerzama@gmail.com>
2022-03-22 17:21:52 +0100

    presentation

--- models/User.model.js
+++ models/User.model.js
@@ -12,17 +12,8 @@ const userSchema = new Schema({
     type: String,
     required: [true, 'password is required'],
   },
-  favoriteArmies: [{ type: [Schema.Types.ObjectId] }],
 });
-
-/*const userSchema = new Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-});*/
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
-
